629fe6e631965a08636eb23b8dc4db89a863984a
bakarali <bakaralisunasra@gmail.com>
2016-01-14 23:25:04 +0530

    added class and read var

---
+++ CPPs/PrayTimes/MainClass.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "prayertimes.hpp"
+using namespace std;
+int main(){
+
+  PrayerTimes ptm;
+//  double default_times[] = { 5, 6, 12, 13, 18, 18, 18 };
+  cout << "" << ptm.test();
+  //cout << "" << ptm.get_prayer_times(2016, 01, 14, 12.9715987, 77.5945627, 5.5, default_times);
+//  printf("%s\n", ptm.get_prayer_times(2016, 01, 14, 12.9715987, 77.5945627, 5.5, default_times);
+}
---
+++ CPPs/namaztime.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+  int a=5,b=5,c=0;
+  c = a + b;
+  printf("%d\n", c);
+  /* code */
+  return 0;
+}
